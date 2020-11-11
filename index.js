cf80025738da2ddb2f00d02395ad2a17a99df7cd
Jon Powell <jpowell68601@gmail.com>
2020-11-10 22:38:28 -0600

    Setup movie search and dropdown component

--- index.js
+++ index.js
@@ -1,1 +1,61 @@
-console.log('Hi there!');
+const fetchData = async (searchTerm) => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: 'ebf85c86',
+            s: searchTerm
+        }
+    })
+
+    if (response.data.Error) {
+        return [];
+    }
+
+    return response.data.Search
+}
+
+const root = document.querySelector('.autocomplete');
+root.innerHTML = `
+    <label><b>Search For a Movie</b></label>
+    <input class="input" type="text">
+    <div class="dropdown">
+        <div class="dropdown-menu">
+            <div class="dropdown-content results"></div>
+        </div>
+    </div>
+`;
+
+const input = document.querySelector('input');
+const dropdown = document.querySelector('.dropdown');
+const resultsWrapper = document.querySelector('.results');
+
+const onInput = async (e) => {
+    const movies = await fetchData(e.target.value)
+
+    if (!movies.length) {
+        dropdown.classList.remove('is-active');
+        return;
+    }
+    
+    resultsWrapper.innerHTML = '';
+    dropdown.classList.add('is-active')
+    for (let movie of movies) {
+        const option = document.createElement('a');
+        const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
+
+        option.classList.add('dropdown-item')
+        option.innerHTML = `
+            <img src="${imgSrc}" />
+            ${movie.Title}
+        `;
+
+        resultsWrapper.appendChild(option);
+    }
+}
+
+input.addEventListener('input', debounce(onInput, 1000))
+
+document.addEventListener('click', (e) => {
+    if (!root.contains(e.target)) {
+        dropdown.classList.remove('is-active')
+    }
+})
